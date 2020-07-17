c318b7b741c6303fb23bb84147b06555dc7aa9bb
Luuk de Vlieger <info@luuk.site>
2020-07-17 11:18:37 +0200

    :zap: Image quality

--- web/static/index.php
+++ web/static/index.php
@@ -8,7 +8,7 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 League\Glide\ServerFactory::create([
     'base_url' => 'static',
     'cache' => $dir . '/web/app/uploads/.cache',
-    'defaults' => ['fit' => 'crop'],
+    'defaults' => ['fit' => 'crop', 'q' => 80],
     'max_image_size' => 3840 * 3840,
     'source' => $dir . '/web/app/uploads',
 ])->outputImage($path, $_GET);

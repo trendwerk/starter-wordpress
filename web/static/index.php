f918fb90c1961ccea0b6d2dd73fb92a9d917caf2
Luuk de Vlieger <info@luuk.site>
2020-07-17 11:02:33 +0200

    :sparkles: Glide config

---
+++ web/static/index.php
@@ -0,0 +1,12 @@
+<?php
+$dir = dirname(__DIR__, 2);
+
+require $dir . '/vendor/autoload.php';
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+League\Glide\ServerFactory::create([
+    'base_url' => 'static',
+    'cache' => $dir . '/web/app/uploads/.glide-cache',
+    'source' => $dir . '/web/app/uploads',
+])->outputImage($path, $_GET);

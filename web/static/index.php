d5707092a2a174989bc56e00ab2a91362aff6483
Luuk de Vlieger <info@luuk.site>
2020-07-17 11:05:17 +0200

    :wrench: Crop fit for images

--- web/static/index.php
+++ web/static/index.php
@@ -4,9 +4,10 @@ $dir = dirname(__DIR__, 2);
 require $dir . '/vendor/autoload.php';
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$args = array_merge($_GET, ['fit' => 'crop']);
 
 League\Glide\ServerFactory::create([
     'base_url' => 'static',
     'cache' => $dir . '/web/app/uploads/.glide-cache',
     'source' => $dir . '/web/app/uploads',
-])->outputImage($path, $_GET);
+])->outputImage($path, $args);

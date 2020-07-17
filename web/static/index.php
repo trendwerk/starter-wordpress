b64b3e054621563f09509ae65af3ec28bee53b86
Luuk de Vlieger <info@luuk.site>
2020-07-17 11:07:57 +0200

    :recycle: Use Glide defaults prop

--- web/static/index.php
+++ web/static/index.php
@@ -4,10 +4,10 @@ $dir = dirname(__DIR__, 2);
 require $dir . '/vendor/autoload.php';
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$args = array_merge($_GET, ['fit' => 'crop']);
 
 League\Glide\ServerFactory::create([
     'base_url' => 'static',
     'cache' => $dir . '/web/app/uploads/.cache',
+    'defaults' => ['fit' => 'crop'],
     'source' => $dir . '/web/app/uploads',
-])->outputImage($path, $args);
+])->outputImage($path, $_GET);

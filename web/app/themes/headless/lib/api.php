cd66ef5021d9e5fa0e5ac71374f317181c55d3fd
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-25 13:18:23 +0200

    :bug: $post is not an instance of WP_POST in register_graphql_field()

--- web/app/themes/headless/lib/api.php
+++ web/app/themes/headless/lib/api.php
@@ -19,7 +19,7 @@ add_action('graphql_register_types', function () {
         register_graphql_field($postTypeName, 'summary', [
             'type' => 'String',
             'resolve' => function ($post) {
-                return get_the_excerpt(get_the_id($post));
+                return get_the_excerpt($post->ID);
             }
         ]);
 
@@ -27,7 +27,7 @@ add_action('graphql_register_types', function () {
         register_graphql_field($postTypeName, 'dateFormatted', [
             'type' => 'String',
             'resolve' => function ($post) {
-                return get_the_date('', get_the_id($post));
+                return get_the_date('', $post->ID);
             }
         ]);
     }

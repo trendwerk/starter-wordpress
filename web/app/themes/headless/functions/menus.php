25d2798c4a700985902b2972e61bdf787bb65cff
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-08-26 12:08:02 +0200

    :wrench: Register footer menu's

--- web/app/themes/headless/functions/menus.php
+++ web/app/themes/headless/functions/menus.php
@@ -4,5 +4,9 @@ namespace Headless;
 // Register menu's
 add_action('init', function () {
     register_nav_menu('main', __('Main menu', 'headless'));
+    register_nav_menu('footer1', __('Footer column 1', 'headless'));
+    register_nav_menu('footer2', __('Footer column 2', 'headless'));
+    register_nav_menu('footer3', __('Footer column 3', 'headless'));
+    register_nav_menu('footer4', __('Footer column 4', 'headless'));
     register_nav_menu('footer', __('Footer menu', 'headless'));
 });

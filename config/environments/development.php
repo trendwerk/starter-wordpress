ca35fa647951c5d6bcedfd4cfb07b06d9db6535a
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2021-02-01 11:55:51 +0100

    Enable debug log

--- config/environments/development.php
+++ config/environments/development.php
@@ -6,10 +6,11 @@
 use Roots\WPConfig\Config;
 
 Config::define('SAVEQUERIES', true);
-Config::define('WP_DEBUG', true);
-Config::define('WP_DEBUG_DISPLAY', true);
-Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DEBUG_LOG', true);
+Config::define('WP_DEBUG', true);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 
 ini_set('display_errors', '1');
 
